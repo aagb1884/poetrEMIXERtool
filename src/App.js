0fd17ec2a5a1f32aa53ff6d3ab4dd1854fbbad1d
aagb1884 <a.a.g.blair@gmail.com>
2024-11-07 21:40:00 +0000

    added credit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <div className='title'>
         <p className='poetr'>poetr</p><p className='emixer'>EMIXER  </p><p className='tool'> Tool</p>
       </div>
+      <aside>by <a href='https://andrewblair.co.uk/'>Andrew Blair</a></aside>
       <MainContainer />
     </div>
   );
